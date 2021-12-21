69030a72045841e18dfa2d46a10a8f1b1fe6d0a3
moransr <96500603+moransr@users.noreply.github.com>
2021-12-21 22:05:44 +0000

    Project Setup

---
+++ main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+covid = pd.read_csv("COVID.csv")
+#This is my code 
+# Choose entries with county Name of Cork.
+Corkonly  = covid[covid['CountyName'] == 'Cork']
+Dublinonly  = covid[covid['CountyName'] == 'Dublin']
+Belfast  = covid[covid['CountyName'] == 'Belfast']
+
+print(Corkonly.to_string())
+print(Dublinonly.to_string())
+print(Belfast.to_string())
